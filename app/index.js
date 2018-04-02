58f044da8a59d4cf57c70a9b3d3bd2a5dcb6d532
Ahmad Nassri <ahmad@ahmadnassri.com>
2018-04-01 23:54:51 -0400

    refactor(options): move options into global command

--- app/index.js
+++ app/index.js
@@ -3,6 +3,24 @@
 const { red, bold } = require('chalk')
 
 require('yargs') // eslint-disable-line no-unused-expressions
+  .options({
+    config: {
+      type: 'strig',
+      description: 'path to greenlight config file'
+    },
+
+    reporter: {
+      type: 'string',
+      description: 'Use the specified output reporter',
+      choices: ['text', 'json', 'html', 'silent'],
+      default: 'text'
+    },
+
+    'soft-exit': {
+      type: 'boolean',
+      description: 'do not exit(1) if issues are found'
+    }
+  })
   .commandDir('commands')
   .demandCommand()
   .help()
